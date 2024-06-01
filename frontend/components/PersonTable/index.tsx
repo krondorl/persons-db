e7012652013a8ee713722b8cb5a039d37d2c9ac2
Adam Burucs <adam.burucs@gmail.com>
2024-06-01 18:27:09 +0200

    feat(frontend): add notempty fn

--- frontend/components/PersonTable/index.tsx
+++ frontend/components/PersonTable/index.tsx
@@ -8,6 +8,10 @@ interface PersonTableProps {
   phone: Phone[];
 }
 
+const notEmpty = (value: number | string | boolean, defaultValue = "") => {
+  return value !== null && value !== undefined ? value : defaultValue;
+};
+
 const PersonTable = ({ base, address, phone }: PersonTableProps) => {
   return (
     <div>
@@ -15,43 +19,43 @@ const PersonTable = ({ base, address, phone }: PersonTableProps) => {
         <tbody>
           <tr>
             <td>Name</td>
-            <td></td>
+            <td>{notEmpty(base.name)}</td>
             <td></td>
             <td></td>
           </tr>
           <tr>
             <td>Place of Birth</td>
-            <td></td>
+            <td>{notEmpty(base.birthPlace)}</td>
             <td></td>
             <td></td>
           </tr>
           <tr>
             <td>Date of Birth</td>
-            <td></td>
+            <td>{notEmpty(base.birthDate)}</td>
             <td></td>
             <td></td>
           </tr>
           <tr>
             <td>Mother&lsquo;s birth name</td>
-            <td></td>
+            <td>{notEmpty(base.motherName)}</td>
             <td></td>
             <td></td>
           </tr>
           <tr>
             <td>Social security number</td>
-            <td></td>
+            <td>{notEmpty(base.socialSecNumber)}</td>
             <td></td>
             <td></td>
           </tr>
           <tr>
             <td>Tax identifier</td>
-            <td></td>
+            <td>{notEmpty(base.taxId)}</td>
             <td></td>
             <td></td>
           </tr>
           <tr>
             <td>Email</td>
-            <td></td>
+            <td>{notEmpty(base.email)}</td>
             <td></td>
             <td></td>
           </tr>
