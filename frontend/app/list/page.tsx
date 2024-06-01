407dee4fe7947bf8e6be5bda9a9d60a77bf55c0f
Adam Burucs <adam.burucs@gmail.com>
2024-06-01 19:54:12 +0200

    feat(frontend): add list fetch and endpoint

--- frontend/app/list/page.tsx
+++ frontend/app/list/page.tsx
@@ -1,4 +1,19 @@
+"use client";
+import { useCallback, useEffect } from "react";
+
 export default function List() {
+  const getList = useCallback((): void => {
+    fetch(`/api/list`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  }, []);
+
+  useEffect(() => {
+    getList();
+  }, []);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="font-bold text-4xl">List person</h1>
