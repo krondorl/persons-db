637de481f1fb88758e92376c01b237262588de6c
Adam Burucs <adam.burucs@gmail.com>
2024-06-02 21:24:37 +0200

    feat(frontend): add div container for new page

--- frontend/app/new/page.tsx
+++ frontend/app/new/page.tsx
@@ -2,6 +2,7 @@ export default function New() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="font-bold text-4xl">New person</h1>
+      <div></div>
     </main>
   );
 }
